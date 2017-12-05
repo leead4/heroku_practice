339e8c7b5e6083c2cd3528a543455062e1b07106
leead4 <leead4@gmail.com>
2017-12-05 17:41:43 -0600

    updated admin

--- blogpost/admin.py
+++ blogpost/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+admin.site.register (Tags)
+admin.site.register (Animation)
+admin.site.register (Asset)
+admin.site.register (Content)
+admin.site.register (Author)
+admin.site.register (Post)
 
-# Register your models here.
+# class TagsAdmin(admin.ModelAdmin):
+# 	fields = ('topic')
+
+# class AnimationAdmin(admin.ModelAdmin):
+# 	fields = ('style')
+
+# class AssetAdmin(admin.ModelAdmin):
+# 	fields = ('file_path','animation','image_data')
+
+# class ContentAdmin(admin.ModelAdmin):
+# 	fields = ('text', 'asset')
+
+# class AuthorAdmin(admin.ModelAdmin):
+# 	fields = ('name', 'avatar')
+
+# class PostAdmin(admin.ModelAdmin):
+# 	fields = ('content', 'title', 'post_like', 'post_type', 'date', 'tags', 'author')
